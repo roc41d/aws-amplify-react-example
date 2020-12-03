2d5d27de9c1ef9e8f7afc5a1ab2d98798d08880b
Rocard <rocard@softtech-inc.com>
2020-12-03 14:20:13 +0100

    modified App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          AWS Amplify CI/CD Demo.
+          AWS Amplify CI/CD Demo
         </p>
         <a
           className="App-link"
